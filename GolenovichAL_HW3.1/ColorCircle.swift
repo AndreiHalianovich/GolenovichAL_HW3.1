882233927686a847f10aa5498c902f187e1b0d07
Andrei Halianovich <igolenovichbox@gmail.com>
2021-03-26 16:33:57 +0300

    HW3.1

---
+++ GolenovichAL_HW3.1/ColorCircle.swift
@@ -0,0 +1,30 @@
+//
+//  ColorCircle.swift
+//  GolenovichAL_HW3.1
+//
+//  Created by Andrei Halianovich on 25.03.21.
+//
+
+import SwiftUI
+
+struct ColorCircle: View {
+    
+    let color: Color
+    var opacity: Double
+
+    
+    var body: some View {
+        Circle()
+            .opacity(opacity)
+            .foregroundColor(color)
+            .frame(width: 120, height: 120)
+            .overlay(Circle().stroke(Color.white, lineWidth: 3))
+            .shadow(radius: 10)
+    }
+}
+
+struct ColorCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorCircle(color: .green, opacity: 1.0)
+    }
+}
--- GolenovichAL_HW3.1/ContentView.swift
+++ GolenovichAL_HW3.1/ContentView.swift
@@ -5,17 +5,75 @@
 //  Created by Andrei Halianovich on 25.03.21.
 //
 
+
 import SwiftUI
 
+enum CurrentLight: Double {
+    case green, orange, red
+}
+
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack{
+            Color(.black)
+                .ignoresSafeArea()
+            VStack{
+                redLight
+                orangeLight
+                greenLight
+                Spacer()
+                buttonAction
+            }
             .padding()
+        }
+    }
+    
+    @State private var buttonText = "START"
+    @State private var currentLight = CurrentLight.red
+    @State private var redLight = ColorCircle(color: .red, opacity: 0.3)
+    @State private var orangeLight = ColorCircle(color: .orange, opacity: 0.3)
+    @State private var greenLight = ColorCircle(color: .green, opacity: 0.3)
+    
+    private var buttonAction: some View {
+        Button(action: {
+            
+            if buttonText == "START" {
+                buttonText = "NEXT"
+            }
+            
+            switch currentLight {
+            case .red:
+                greenLight.opacity = 0.3
+                redLight.opacity = 1.0
+                currentLight = .orange
+            case .orange:
+                redLight.opacity = 0.3
+                orangeLight.opacity = 1.0
+                currentLight = .green
+            case .green:
+                greenLight.opacity = 1.0
+                orangeLight.opacity = 0.3
+                currentLight = .red
+            }
+        }) {
+            Text(buttonText)
+                .padding(.horizontal)
+                .background(Color.blue)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.white, lineWidth: 3)
+                )
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPod touch (7th generation)")
     }
 }
